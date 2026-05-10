40f1890ade8af5d1a006b42eb11978838d970479
Jorge Sanchez <jorgeluissanchezbarreneche@gmail.com>
2026-05-09 19:00:17 -0500

    fix: adjust responsive behavior for SVG components on landing screen

--- src/app/(auth)/landing.tsx
+++ src/app/(auth)/landing.tsx
@@ -16,10 +16,10 @@ const { width } = Dimensions.get("window");
     <View className="flex-1 w-full bg-[#F5F2EC]">
       <Text className="text-lg p-10 text-center text-gray-400 tracking-wide">UniMejores</Text>
       <View className="flex-row a justify-center gap-10 mt-12">
-        <SvgXml xml={HOMBRE_SVG} width="150" height="200" style={{transform: [{ translateY: width < 400 ? 70 : 0 }] }} />
-        <SvgXml xml={MUJER_SVG} width="150" height="200" style={{transform: [{ translateY: width < 400 ? 70 : 0 }] }} />
+        <SvgXml xml={HOMBRE_SVG} width="150" height="200" style={{transform: [{ translateY: width < 450 ? 70 : 0 }] }} />
+        <SvgXml xml={MUJER_SVG} width="150" height="200" style={{transform: [{ translateY: width < 450 ? 70 : 0 }] }} />
       </View>
-      {width < 400 && <SvgXml width={width} height={width / 4} xml={BG_SVG} className="translate-y-5" /> }
+      {width < 450 && <SvgXml width={width} height={width / 4} xml={BG_SVG} className="translate-y-5" /> }
       <View className="relative bg-white flex-1 px-8 pt-6 pb-12 gap-5 items-center justify-center">
         <View className="gap-2 w-full max-w-lg">
           <Text className="text-3xl text-gray-800 text-center">
